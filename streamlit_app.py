e53e6b20ccb7fe833bb0a1680db07a42c19d9b72
Raymond Smit <46483889+Raymond-Smit@users.noreply.github.com>
2023-06-16 13:56:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast menu')
+
+streamlit.text('Omega3 &  Blueberry Oatmeal')
+
+streamlit.text('Kale, Spinanch & Rocket Smoothie')
+
+streamlit.text('Hard-boiled Free-Range Egg')
