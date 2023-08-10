599ff70d85532f30bf186f5ea7e01d5e23c7c145
Raymond Smit <46483889+Raymond-Smit@users.noreply.github.com>
2023-08-10 19:18:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Colin menu')
+streamlit.header('Maxims menu')
 streamlit.snow()
 streamlit.text('🥣 Omega3 &  Blueberry Oatmeal')
 
