1b0f4de11853a245532b374463eedc39d6c30023
Raymond Smit <46483889+Raymond-Smit@users.noreply.github.com>
2023-09-22 11:43:22 +0200

    Create Map.py

---
+++ pages/Map.py
@@ -0,0 +1,18 @@
+import folium
+
+# Create a map centered at a specific location
+m = folium.Map(location=[latitude, longitude], zoom_start=10)
+
+# Add a marker to the map
+folium.Marker(
+    location=[latitude, longitude],
+    popup="Marker Popup Text",
+    icon=folium.Icon(color='blue')
+).add_to(m)
+
+# Save the map to an HTML file
+m.save('map.html')
+
+# Open the map in a web browser
+import webbrowser
+webbrowser.open('map.html')
